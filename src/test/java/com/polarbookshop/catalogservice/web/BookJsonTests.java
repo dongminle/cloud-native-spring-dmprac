75318a566f9100a52ceb9840ca09a339d99f24e3
pkalsh <pkalsh345@gmail.com>
2024-05-18 15:15:41 +0900

    [enh]fix github workflow

--- src/test/java/com/polarbookshop/catalogservice/web/BookJsonTests.java
+++ src/test/java/com/polarbookshop/catalogservice/web/BookJsonTests.java
@@ -11,16 +11,18 @@ import java.time.Instant;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @JsonTest
-public class BookJsonTests {
+class BookJsonTests {
 
     @Autowired
     private JacksonTester<Book> json;
 
     @Test
     void testSerialize() throws Exception {
-        var book = Book.of("1234567890", "Title", "Author", 9.90, "Publisher");
+        var now = Instant.now();
+        var book = new Book(394L, "1234567890", "Title", "Author", 9.90, "Polarsophia", now, now, 21);
         var jsonContent = json.write(book);
-
+        assertThat(jsonContent).extractingJsonPathNumberValue("@.id")
+                .isEqualTo(book.id().intValue());
         assertThat(jsonContent).extractingJsonPathStringValue("@.isbn")
                 .isEqualTo(book.isbn());
         assertThat(jsonContent).extractingJsonPathStringValue("@.title")
@@ -37,28 +39,27 @@ public class BookJsonTests {
                 .isEqualTo(book.lastModifiedDate().toString());
         assertThat(jsonContent).extractingJsonPathNumberValue("@.version")
                 .isEqualTo(book.version());
-
     }
 
     @Test
     void testDeserialize() throws Exception {
-        var instant = Instant.parse("2024-04-18T15:50:37.135029Z");
+        var instant = Instant.parse("2021-09-07T22:50:37.135029Z");
         var content = """
                 {
-                    "id": 123,
+                    "id": 394,
                     "isbn": "1234567890",
                     "title": "Title",
                     "author": "Author",
-                    "price": "9.90",
-                    "publisher": "Publisher",
-                    "createdDate": "2024-04-18T15:50:37.135029Z",
-                    "lastModifiedDate": "2024-04-18T15:50:37.135029Z",
+                    "price": 9.90,
+                    "publisher": "Polarsophia",
+                    "createdDate": "2021-09-07T22:50:37.135029Z",
+                    "lastModifiedDate": "2021-09-07T22:50:37.135029Z",
                     "version": 21
                 }
                 """;
-
         assertThat(json.parse(content))
                 .usingRecursiveComparison()
-                .isEqualTo(new Book(123L, "1234567890", "Title", "Author", 9.90, "Publisher", instant, instant, 21));
+                .isEqualTo(new Book(394L, "1234567890", "Title", "Author", 9.90, "Polarsophia", instant, instant, 21));
     }
+
 }
