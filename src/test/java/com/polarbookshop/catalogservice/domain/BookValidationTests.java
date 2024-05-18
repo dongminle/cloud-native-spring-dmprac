27d4b3b71a9253e258e811c4a95dc7b49983de94
pkalsh <pkalsh345@gmail.com>
2024-05-18 14:34:40 +0900

    [enh]Add Dockerfile

--- src/test/java/com/polarbookshop/catalogservice/domain/BookValidationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/domain/BookValidationTests.java
@@ -9,7 +9,9 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class BookValidationTests {
     private static Validator validator;
@@ -28,11 +30,75 @@ public class BookValidationTests {
     }
 
     @Test
+    void whenIsbnNotDefinedThenValidationFails() {
+        var book = Book.of("", "Title", "Author", 9.90, "Publisher");
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations).hasSize(2);
+        List<String> constraintViolationMessages = violations.stream()
+                .map(ConstraintViolation::getMessage).collect(Collectors.toList());
+        assertThat(constraintViolationMessages)
+                .contains("The book ISBN must be defined.")
+                .contains("The ISBN format must be valid.");
+    }
+
+    @Test
     void whenIsbnDefinedButIncorrectThenValidationFails() {
         var book = Book.of("a234567890", "Title", "Author", 9.90, "Publisher");
         Set<ConstraintViolation<Book>> violations = validator.validate(book);
         assertThat(violations).hasSize(1);
         assertThat(violations.iterator().next().getMessage())
                 .isEqualTo("The ISBN format must be valid.");
+    }
+
+    @Test
+    void whenTitleIsNotDefinedThenValidationFails() {
+        var book = Book.of("1234567890", "", "Author", 9.90, "Publisher");
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations).hasSize(1);
+        assertThat(violations.iterator().next().getMessage())
+                .isEqualTo("The book title must be defined.");
+    }
+
+    @Test
+    void whenAuthorIsNotDefinedThenValidationFails() {
+        var book = Book.of("1234567890", "Title", "", 9.90, "Publisher");
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations).hasSize(1);
+        assertThat(violations.iterator().next().getMessage())
+                .isEqualTo("The book author must be defined.");
+    }
+
+    @Test
+    void whenPriceIsNotDefinedThenValidationFails() {
+        var book = Book.of("1234567890", "Title", "Author", null, "Publisher");
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations).hasSize(1);
+        assertThat(violations.iterator().next().getMessage())
+                .contains("The book price must be defined.");
+    }
+
+    @Test
+    void whenPriceIsDefinedButZeroThenValidationFails() {
+        var book = Book.of("1234567890", "Title", "Author", 0.0, "Publisher");
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations).hasSize(1);
+        assertThat(violations.iterator().next().getMessage())
+                .isEqualTo("The book price must be greater than zero.");
+    }
+
+    @Test
+    void whenPriceDefinedButNegativeThenValidationFails() {
+        var book = Book.of("1234567890", "Title", "Author", -9.90, "Publisher");
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations).hasSize(1);
+        assertThat(violations.iterator().next().getMessage())
+                .isEqualTo("The book price must be greater than zero.");
+    }
+
+    @Test
+    void whenPublisherIsNotDefinedThenValidationSucceeds() {
+        var book = Book.of("1234567890", "Title", "Author", 9.90, null);
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations).isEmpty();
     }
 }
--- src/test/java/com/polarbookshop/catalogservice/web/BookJsonTests.java
+++ src/test/java/com/polarbookshop/catalogservice/web/BookJsonTests.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
 
+import java.time.Instant;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @JsonTest
@@ -29,22 +31,34 @@ public class BookJsonTests {
                 .isEqualTo(book.price());
         assertThat(jsonContent).extractingJsonPathStringValue("@.publisher")
                 .isEqualTo(book.publisher());
+        assertThat(jsonContent).extractingJsonPathStringValue("@.createdDate")
+                .isEqualTo(book.createdDate().toString());
+        assertThat(jsonContent).extractingJsonPathStringValue("@.lastModifiedDate")
+                .isEqualTo(book.lastModifiedDate().toString());
+        assertThat(jsonContent).extractingJsonPathNumberValue("@.version")
+                .isEqualTo(book.version());
+
     }
 
     @Test
     void testDeserialize() throws Exception {
+        var instant = Instant.parse("2024-04-18T15:50:37.135029Z");
         var content = """
                 {
+                    "id": 123,
                     "isbn": "1234567890",
                     "title": "Title",
                     "author": "Author",
                     "price": "9.90",
-                    "publisher": "Publisher"
+                    "publisher": "Publisher",
+                    "createdDate": "2024-04-18T15:50:37.135029Z",
+                    "lastModifiedDate": "2024-04-18T15:50:37.135029Z",
+                    "version": 21
                 }
                 """;
 
         assertThat(json.parse(content))
                 .usingRecursiveComparison()
-                .isEqualTo(Book.of("1234567890", "Title", "Author", 9.90, "Publisher"));
+                .isEqualTo(new Book(123L, "1234567890", "Title", "Author", 9.90, "Publisher", instant, instant, 21));
     }
 }
